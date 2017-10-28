7a6aa1b50f902b3ac27527d1523b58adfb3efe31
Drakkanrider <drakkanrider@gmail.com>
2017-10-28 10:31:24 -0700

    OOPs
    oops, I OOPed

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
-DECK_SIZE = 25
-NUM_OF_SHIT = 3
-
 class Deck:
   size = 0
   deckList = {}
+  numSilvers = 0
+  numGolds = 0
+  numBronzes = 0
+  def printDeck(self):
+    for key in self.deckList:
+      print(key, end=" ")
+      print(self.deckList[key])
+      
+  def load(self, file):
+    with open(file) as f:
+      for line in f:
+        if line[-1] == '\n':
+          deck.deckList[line[2:-1]] = line[0]
+        else:
+          deck.deckList[line[2:]] = line[0]
+        deck.size = deck.size + int(line[0])
+  
+class Card:
+  name = ''
+  strength = 0
+  group = ''
+  rarity = ''
+  loyalty = ''
+  faction = ''
+  position = ''
+  types = []
+  text = ''
+  mechanics = []
 
 #HYPERGEOMETRIC CALCULATIONS
 def draw(numCards, deckSize):
@@ -18,17 +43,6 @@ def handDraw(numCards, deckSize):
 
 #pedestrian file input
 deck = Deck()
-with open('deck.txt') as f:
-  for line in f:
-    if line[-1] == '\n':
-      deck.deckList[line[2:-1]] = line[0]
-    else:
-      deck.deckList[line[2:]] = line[0]
-    deck.size = deck.size + int(line[0])
-
-#test printing    
-for key in deck.deckList:
-  print(key, end=" ")
-  print(deck.deckList[key])
+deck.load('deck.txt')
   
-print (deck.size)
+deck.printDeck()
